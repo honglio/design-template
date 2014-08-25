6ea5dd7845d17993e4bb8d75d1775018553265e9
honglio <hong.liu@live.cn>
2014-08-25 13:44:57 +0800

    add all module pages.

--- config/routes.js
+++ config/routes.js
@@ -25,6 +25,38 @@ var index = require('../server/controllers/index');
 module.exports = function (app, passport) {
 
     app.get('/', index.home);
+    app.get('/home-2-1', index.home_2_1);
+    app.get('/home-3-1', index.home_3_1);
+    app.get('/home-4-1', index.home_4_1);
+    app.get('/home-5-1', index.home_5_1);
+    app.get('/home-2-2', index.home_2_2);
+    app.get('/home-3-2', index.home_3_2);
+    app.get('/home-4-2', index.home_4_2);
+    app.get('/home-5-2', index.home_5_2);
+
+    app.get('/module-advert-1', index.module_advert_1);
+    app.get('/module-advert-2', index.module_advert_2);
+    app.get('/module-advert-3', index.module_advert_3);
+    app.get('/module-advert-4', index.module_advert_4);
+    app.get('/module-advert-5', index.module_advert_5);
+    app.get('/module-advert-6', index.module_advert_6);
+    app.get('/module-advert-7', index.module_advert_7);
+
+    app.get('/module-blog-1', index.module_blog_1);
+    app.get('/module-blog-2', index.module_blog_2);
+
+    app.get('/module-portfolio-1', index.module_portfolio_1);
+    app.get('/module-portfolio-2', index.module_portfolio_2);
+
+    app.get('/module-service-1', index.module_service_1);
+    app.get('/module-service-2', index.module_service_2);
+    app.get('/module-service-3', index.module_service_3);
+    app.get('/module-service-4', index.module_service_4);
+
+    app.get('/module-client', index.module_client);
+    app.get('/module-price', index.module_price);
+    app.get('/module-team', index.module_team);
+    app.get('/module-twitter', index.module_twitter);
 
     // page not found
     app.get('*', index.notfound);
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -16,6 +16,303 @@ exports.home = function(req, res) {
 };
 
 /**
+ * GET /home-2-1
+ * Home page.
+ */
+
+exports.home_2_1 = function(req, res) {
+  res.render('home-2-1', {
+    title: '首页'
+  });
+};
+
+/**
+ * GET /home-3-1
+ * Home page.
+ */
+
+exports.home_3_1 = function(req, res) {
+  res.render('home-3-1', {
+    title: '首页'
+  });
+};
+
+/**
+ * GET /home-4-1
+ * Home page.
+ */
+
+exports.home_4_1 = function(req, res) {
+  res.render('home-4-1', {
+    title: '首页'
+  });
+};
+
+/**
+ * GET /home-5-1
+ * Home page.
+ */
+
+exports.home_5_1 = function(req, res) {
+  res.render('home-5-1', {
+    title: '首页'
+  });
+};
+
+/**
+ * GET /home-2-2
+ * Home page.
+ */
+
+exports.home_2_2 = function(req, res) {
+  res.render('home-2-2', {
+    title: '首页'
+  });
+};
+
+/**
+ * GET /home-3-2
+ * Home page.
+ */
+
+exports.home_3_2 = function(req, res) {
+  res.render('home-3-2', {
+    title: '首页'
+  });
+};
+
+/**
+ * GET /home-4-2
+ * Home page.
+ */
+
+exports.home_4_2 = function(req, res) {
+  res.render('home-4-2', {
+    title: '首页'
+  });
+};
+
+/**
+ * GET /home-5-2
+ * Home page.
+ */
+
+exports.home_5_2 = function(req, res) {
+  res.render('home-5-2', {
+    title: '首页'
+  });
+};
+
+/**
+ * GET /module-advert-1
+ * Module advert page.
+ */
+
+exports.module_advert_1 = function(req, res) {
+  res.render('module-advert-1', {
+    title: '模块-广告'
+  });
+};
+
+/**
+ * GET /module-advert-2
+ * Module advert page.
+ */
+
+exports.module_advert_2 = function(req, res) {
+  res.render('module-advert-2', {
+    title: '模块-广告'
+  });
+};
+
+/**
+ * GET /module-advert-3
+ * Module advert page.
+ */
+
+exports.module_advert_3 = function(req, res) {
+  res.render('module-advert-3', {
+    title: '模块-广告'
+  });
+};
+
+/**
+ * GET /module-advert-4
+ * Module advert page.
+ */
+
+exports.module_advert_4 = function(req, res) {
+  res.render('module-advert-4', {
+    title: '模块-广告'
+  });
+};
+
+/**
+ * GET /module-advert-5
+ * Module advert page.
+ */
+
+exports.module_advert_5 = function(req, res) {
+  res.render('module-advert-5', {
+    title: '模块-广告'
+  });
+};
+
+/**
+ * GET /module-advert-6
+ * Module advert page.
+ */
+
+exports.module_advert_6 = function(req, res) {
+  res.render('module-advert-6', {
+    title: '模块-广告'
+  });
+};
+
+/**
+ * GET /module-advert-7
+ * Module advert page.
+ */
+
+exports.module_advert_7 = function(req, res) {
+  res.render('module-advert-7', {
+    title: '模块-广告'
+  });
+};
+
+/**
+ * GET /module-blog-1
+ * Module blog page.
+ */
+
+exports.module_blog_1 = function(req, res) {
+  res.render('module-blog-1', {
+    title: '模块-blog'
+  });
+};
+
+/**
+ * GET /module-blog-2
+ * Module blog page.
+ */
+
+exports.module_blog_2 = function(req, res) {
+  res.render('module-blog-2', {
+    title: '模块-blog'
+  });
+};
+
+/**
+ * GET /module-portfolio-1
+ * Module portfolio page.
+ */
+
+exports.module_portfolio_1 = function(req, res) {
+  res.render('module-portfolio-1', {
+    title: '模块-portfolio'
+  });
+};
+
+/**
+ * GET /module-portfolio-2
+ * Module portfolio page.
+ */
+
+exports.module_portfolio_2 = function(req, res) {
+  res.render('module-portfolio-2', {
+    title: '模块-portfolio'
+  });
+};
+
+/**
+ * GET /module-service-1
+ * Module service page.
+ */
+
+exports.module_service_1 = function(req, res) {
+  res.render('module-service-1', {
+    title: '模块-service'
+  });
+};
+
+/**
+ * GET /module-service-2
+ * Module service page.
+ */
+
+exports.module_service_2 = function(req, res) {
+  res.render('module-service-2', {
+    title: '模块-service'
+  });
+};
+
+/**
+ * GET /module-service-3
+ * Module service page.
+ */
+
+exports.module_service_3 = function(req, res) {
+  res.render('module-service-3', {
+    title: '模块-service'
+  });
+};
+
+/**
+ * GET /module-service-4
+ * Module service page.
+ */
+
+exports.module_service_4 = function(req, res) {
+  res.render('module-service-4', {
+    title: '模块-service'
+  });
+};
+
+/**
+ * GET /module-client
+ * Module client page.
+ */
+
+exports.module_client = function(req, res) {
+  res.render('module-client', {
+    title: '模块-client'
+  });
+};
+
+/**
+ * GET /module-price
+ * Module price page.
+ */
+
+exports.module_price = function(req, res) {
+  res.render('module-price', {
+    title: '模块-price'
+  });
+};
+
+/**
+ * GET /module-team
+ * Module team page.
+ */
+
+exports.module_team = function(req, res) {
+  res.render('module-team', {
+    title: '模块-team'
+  });
+};
+
+/**
+ * GET /module-twitter
+ * Module twitter page.
+ */
+
+exports.module_twitter = function(req, res) {
+  res.render('module-twitter', {
+    title: '模块-twitter'
+  });
+};
+
+/**
  * GET /login
  * Login page.
  */
