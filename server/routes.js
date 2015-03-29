802383bb360c764d58521600f6e7b811cf9aee9a
honglio <hong.liu@live.cn>
2015-03-29 22:38:13 +0800

    Removed bad page.

--- server/routes.js
+++ server/routes.js
@@ -34,61 +34,6 @@ module.exports = function (app, passport) {
     app.get('/logout', index.logout);
 
     app.get('/index.html', passportConf.isAuthenticated, index.home);
-    app.get('/home-2-1.html', passportConf.isAuthenticated, index.home_2_1);
-    app.get('/home-3-1.html', passportConf.isAuthenticated, index.home_3_1);
-    app.get('/home-4-1.html', passportConf.isAuthenticated, index.home_4_1);
-    app.get('/home-5-1.html', passportConf.isAuthenticated, index.home_5_1);
-    app.get('/home-2-2.html', passportConf.isAuthenticated, index.home_2_2);
-    app.get('/home-3-2.html', passportConf.isAuthenticated, index.home_3_2);
-    app.get('/home-4-2.html', passportConf.isAuthenticated, index.home_4_2);
-    app.get('/home-5-2.html', passportConf.isAuthenticated, index.home_5_2);
-
-    app.get('/module-advert-1.html', passportConf.isAuthenticated, index.module_advert_1);
-    app.get('/module-advert-2.html', passportConf.isAuthenticated, index.module_advert_2);
-    app.get('/module-advert-3.html', passportConf.isAuthenticated, index.module_advert_3);
-    app.get('/module-advert-4.html', passportConf.isAuthenticated, index.module_advert_4);
-    app.get('/module-advert-5.html', passportConf.isAuthenticated, index.module_advert_5);
-    app.get('/module-advert-6.html', passportConf.isAuthenticated, index.module_advert_6);
-
-    app.get('/module-slider-1.html', passportConf.isAuthenticated, index.module_slider_1);
-    app.get('/module-slider-2.html', passportConf.isAuthenticated, index.module_slider_2);
-    app.get('/module-slider-3.html', passportConf.isAuthenticated, index.module_slider_3);
-    app.get('/module-slider-4.html', passportConf.isAuthenticated, index.module_slider_4);
-
-    app.get('/module-mixitup-1.html', passportConf.isAuthenticated, index.module_mixitup_1);
-    app.get('/module-mixitup-2.html', passportConf.isAuthenticated, index.module_mixitup_2);
-
-    app.get('/module-service-1.html', passportConf.isAuthenticated, index.module_service_1);
-    app.get('/module-service-2.html', passportConf.isAuthenticated, index.module_service_2);
-    app.get('/module-service-3.html', passportConf.isAuthenticated, index.module_service_3);
-    app.get('/module-service-4.html', passportConf.isAuthenticated, index.module_service_4);
-
-    app.get('/module-client.html', passportConf.isAuthenticated, index.module_client);
-    app.get('/module-price.html', passportConf.isAuthenticated, index.module_price);
-    app.get('/module-team.html', passportConf.isAuthenticated, index.module_team);
-    app.get('/module-twitter.html', passportConf.isAuthenticated, index.module_twitter);
-
-    app.get('/page-about-1.html', passportConf.isAuthenticated, index.page_about_1);
-    app.get('/page-about-2.html', passportConf.isAuthenticated, index.page_about_2);
-    app.get('/page-about-3.html', passportConf.isAuthenticated, index.page_about_3);
-    app.get('/page-about-4.html', passportConf.isAuthenticated, index.page_about_4);
-
-    app.get('/page-services-1.html', passportConf.isAuthenticated, index.page_services_1);
-    app.get('/page-services-2.html', passportConf.isAuthenticated, index.page_services_2);
-
-    app.get('/page-portfolio-single-1.html', passportConf.isAuthenticated, index.page_portfolio_single_1);
-    app.get('/page-portfolio-single-2.html', passportConf.isAuthenticated, index.page_portfolio_single_2);
-
-    app.get('/page-contact.html', passportConf.isAuthenticated, index.page_contact);
-
-    app.get('/blog-1.html', passportConf.isAuthenticated, index.blog_1);
-    app.get('/blog-2.html', passportConf.isAuthenticated, index.blog_2);
-    app.get('/blog-3.html', passportConf.isAuthenticated, index.blog_3);
-
-    app.get('/blog-single-1.html', passportConf.isAuthenticated, index.blog_single_1);
-    app.get('/blog-single-2.html', passportConf.isAuthenticated, index.blog_single_2);
-    app.get('/blog-single-3.html', passportConf.isAuthenticated, index.blog_single_3);
-    app.get('/blog-single-4.html', passportConf.isAuthenticated, index.blog_single_4);
 
     app.get('/shop.html', passportConf.isAuthenticated, index.shop);
     app.get('/shop-detail.html', passportConf.isAuthenticated, index.shop_detail);
