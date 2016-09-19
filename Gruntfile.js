78ccb6cbabb7feda4e34e98a7c86eeb9490ddc6a
Hong Liu <hong.liu@live.cn>
2016-09-20 00:08:56 +0800

    Fixed bugs

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,28 +4,27 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         lesslint: {
-            src: 'less/*.less',
+            src: ['less/app.less'],
             options: {
                 csslint: {
                     csslintrc: '.csslintrc'
-                }
+                },
+                imports: ['less/*.less']
             }
         },
 
         less: {
-            files: {
-                'dist/app.css': 'less/app.less'
+            development: {
+                files: {
+                    'dist/app.css': 'less/app.less'
+                }
             }
         }
         
     });
 
-    grunt.registerTask('less2css', [
+    grunt.registerTask('default', [
         'lesslint',
         'less'
-    ]);
-
-    grunt.registerTask('default', [
-        'less2css'
     ]);
 };
